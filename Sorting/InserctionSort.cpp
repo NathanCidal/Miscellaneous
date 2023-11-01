93bacbf7eb404c2f3adac6eac2551105b4647e34
NathanCidal <143615223+NathanCidal@users.noreply.github.com>
2023-11-01 09:04:21 -0300

    --Fixed some issues--

--- Sorting/InserctionSort.cpp
+++ Sorting/InserctionSort.cpp
@@ -2,12 +2,12 @@
 using namespace std;
 void InserctionSort(int * v, int t){
     for(int i = 1; i < t; i++){
-        int base = i;
+        int base = v[i];
         int j = i - 1;
-        while(j >= 0 && v[j] > v[base]){
+        while(j >= 0 && v[j] > base){
             v[j+1] = v[j];
             j--;
         }
-        v[j+1] = v[base];
+        v[j+1] = base;
     }
 }
